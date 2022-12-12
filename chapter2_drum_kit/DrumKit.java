7cf7acb935fa006f9e4a2dda27a7abcaf472041c
Dzmitry Buracheuski <96749989+DzmitryBur@users.noreply.github.com>
2022-12-12 15:33:16 +0300

    Add files via upload

---
+++ chapter2_drum_kit/DrumKit.java
@@ -0,0 +1,15 @@
+package chapter2_drum_kit;
+
+public class DrumKit {
+
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat() {
+        System.out.println("���� ���� ��-����");
+    }
+
+    void playSnare() {
+        System.out.println("��� ��� ��-���");
+    }
+}
