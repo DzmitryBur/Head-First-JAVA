420f4d8f28077619b823cfac9c5bdd88c92dea32
Dzmitry Buracheuski <96749989+DzmitryBur@users.noreply.github.com>
2022-12-18 23:54:54 +0300

    Add files via upload

---
+++ chapter5_multi_for/MultiFor.java
@@ -0,0 +1,18 @@
+package chapter5_multi_for;
+
+public class MultiFor {
+
+    public static void main(String[] args) {
+
+        for (int x = 0; x < 4; x++) {
+            for (int y = 4; y > 2; y--) {
+                System.out.println(x + " " + y);
+            }
+            if (x == 1) {
+                x++;
+            }
+        }
+
+    }
+
+}
